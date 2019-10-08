a1802234008cdaa675e1d10436b1d799eeaf810a
Deivison Cardoso <deivisondc@gmail.com>
2019-10-08 20:43:47 -0300

    typo

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -6,14 +6,14 @@ import camera from '../../assets/camera.svg';
 import './styles.css';
 
 export default function New({ history }) {
-  const [thumbmail, setThumbmail] = useState(null);
+  const [thumbnail, setThumbnail] = useState(null);
   const [company, setCompany] = useState('');
   const [techs, setTechs] = useState('');
   const [price, setPrice] = useState('');
 
   const preview = useMemo(() => {
-    return thumbmail ? URL.createObjectURL(thumbmail) : null;
-  }, [thumbmail])
+    return thumbnail ? URL.createObjectURL(thumbnail) : null;
+  }, [thumbnail])
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -21,7 +21,7 @@ export default function New({ history }) {
     const data = new FormData();
     const userId = localStorage.getItem('user');
 
-    data.append('thumbmail', thumbmail);
+    data.append('thumbnail', thumbnail);
     data.append('company', company);
     data.append('techs', techs);
     data.append('price', price);
@@ -36,12 +36,12 @@ export default function New({ history }) {
   return (
     <form onSubmit={handleSubmit}>
       <label 
-        id="thumbmail" 
+        id="thumbnail" 
         style={{ backgroundImage: `url(${preview})`}}
-        className={thumbmail ? 'has-thumbmail' : ''}
+        className={thumbnail ? 'has-thumbnail' : ''}
       >
-        <input type="file" onChange={event => setThumbmail(event.target.files[0])}/>
-        <img src={camera} alt="Select image" />
+        <input type="file" onChange={event => setThumbnail(event.target.files[0])}/>
+        <img src={camera} alt="Select" />
       </label>
 
       <label htmlFor="company">EMPRESA *</label>
